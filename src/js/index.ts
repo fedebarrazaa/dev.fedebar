6334eaf15692e21e776a4c6be1b6f789d3ef37b0
fedebarrazaa <fedebarraza97@gmail.com>
2025-11-10 19:58:02 -0300

    feat: Implementa tooltip con CSS

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,12 +1,1 @@
-//SE USO SOLAMENTE PARA MOSTRAR LA IMG
-/*import imgMe from "../assets/img_me.jpg";
-
-const cardImg = document.querySelector<HTMLImageElement>(".card__img");
-
-if (cardImg) {
-  cardImg.src = imgMe;
-  console.log("✅ Imagen asignada:", imgMe);
-} else {
-  console.log("❌ No se encontró el elemento .card__img");
-}
-*/
+//NO SE USO HASTA EL MOMENTO
